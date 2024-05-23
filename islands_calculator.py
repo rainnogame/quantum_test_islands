d185b686c07c7d08e41b2de7cdc4ea1266c3aba6
Yevhenii Zahrebelnyi <yevhenii.zahrebelnyi@gmail.com>
2024-05-23 15:55:36 +0300

    Add \n to gitignore file. Improve formatting using black

--- islands_calculator.py
+++ islands_calculator.py
@@ -2,7 +2,7 @@ import numpy as np
 
 
 def visit_island(
-        islands_matrix: np.ndarray, row_to_visit: int, column_to_visit: int
+    islands_matrix: np.ndarray, row_to_visit: int, column_to_visit: int
 ) -> None:
     """
     Visit the island recursively
@@ -14,11 +14,11 @@ def visit_island(
     """
 
     if (
-            row_to_visit < 0
-            or row_to_visit >= islands_matrix.shape[0]
-            or column_to_visit < 0
-            or column_to_visit >= islands_matrix.shape[1]
-            or islands_matrix[row_to_visit][column_to_visit] == 0
+        row_to_visit < 0
+        or row_to_visit >= islands_matrix.shape[0]
+        or column_to_visit < 0
+        or column_to_visit >= islands_matrix.shape[1]
+        or islands_matrix[row_to_visit][column_to_visit] == 0
     ):
         # Check out of bounds or not an island
         # 0 in islands_matrix means that cell is not an island or the cell is already visited
@@ -34,7 +34,7 @@ def visit_island(
 
 
 def get_islands_count_recursive(
-        islands_matrix_shape: tuple[int, int], islands_matrix: np.ndarray
+    islands_matrix_shape: tuple[int, int], islands_matrix: np.ndarray
 ) -> int:
     """
     Get the number of islands in the 2D matrix
--- test_islands_calculator.py
+++ test_islands_calculator.py
@@ -3,22 +3,43 @@ import numpy as np
 import pytest
 
 
-@pytest.mark.parametrize("shape,matrix,output", [
-    ((3, 3), np.array([
-        [0, 1, 0],
-        [0, 0, 0],
-        [0, 1, 1],
-    ]), 2),
-    ((3, 4), np.array([
-        [0, 0, 0, 1],
-        [0, 0, 1, 0],
-        [0, 1, 0, 0],
-    ]), 3),
-    ((3, 4), np.array([
-        [0, 0, 0, 1],
-        [0, 0, 1, 1],
-        [0, 1, 0, 1],
-    ]), 2),
-])
+@pytest.mark.parametrize(
+    "shape,matrix,output",
+    [
+        (
+            (3, 3),
+            np.array(
+                [
+                    [0, 1, 0],
+                    [0, 0, 0],
+                    [0, 1, 1],
+                ]
+            ),
+            2,
+        ),
+        (
+            (3, 4),
+            np.array(
+                [
+                    [0, 0, 0, 1],
+                    [0, 0, 1, 0],
+                    [0, 1, 0, 0],
+                ]
+            ),
+            3,
+        ),
+        (
+            (3, 4),
+            np.array(
+                [
+                    [0, 0, 0, 1],
+                    [0, 0, 1, 1],
+                    [0, 1, 0, 1],
+                ]
+            ),
+            2,
+        ),
+    ],
+)
 def test_get_islands_count_2d(shape, matrix, output):
     assert get_islands_count_recursive(shape, matrix) == output
